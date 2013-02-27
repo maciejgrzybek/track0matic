0d733217b661bf035dd103217f48c78fb88496cc
Maciej 'mac' Grzybek <mashester@gmail.com>
2013-02-27 10:19:50 +0100

    First implementation of ResultComparators (And and Or ones).

--- src/Model/resultcomparator.cpp
+++ src/Model/resultcomparator.cpp
@@ -1,5 +1,67 @@
+#include <stdexcept>
+
 #include "resultcomparator.h"
 
-ResultComparator::ResultComparator()
+ResultComparator::ResultComparator(const feature_grade_map_t& gradeRates)
+  : gradeRates_(gradeRates)
 {
+}
+
+AndComparator::AndComparator(const feature_grade_map_t& gradeRates)
+  : ResultComparator(gradeRates)
+{}
+
+double AndComparator::operator()(const feature_grade_map_t& featureGrades)
+{
+  return evaluateGrades(featureGrades);
+}
+
+double AndComparator::evaluateGrades(const feature_grade_map_t& featureGrades)
+{
+  double result = 1;
+  for (auto& feature : featureGrades)
+  {
+    feature_grade_map_t::mapped_type gradeRate = 0;
+    try
+    {
+      gradeRate = gradeRates_.at(feature.first);
+    }
+    catch (const std::out_of_range&)
+    {
+      // nothing do to here, gradeRate already set to 0, on initialization
+    }
+    result *= (gradeRate*feature.second);
+  }
+  return result;
+}
+
+
+OrComparator::OrComparator(const feature_grade_map_t& gradeRates)
+  : ResultComparator(gradeRates)
+{}
+
+double OrComparator::operator()(const feature_grade_map_t& featureGrades)
+{
+  return evaluateGrades(featureGrades);
+}
+
+double OrComparator::evaluateGrades(const feature_grade_map_t& featureGrades)
+{
+  int i = 0;
+  double result = 0;
+  for (auto& feature : featureGrades)
+  {
+    feature_grade_map_t::mapped_type gradeRate = 0;
+    try
+    {
+      gradeRate = gradeRates_.at(feature.first);
+    }
+    catch (const std::out_of_range&)
+    {
+      // nothing do to here, gradeRate already set to 0, on initialization
+    }
+    result += (gradeRate*feature.second);
+    ++i;
+  }
+  return result/i; // normalize to number of features
 }
--- src/Model/resultcomparator.h
+++ src/Model/resultcomparator.h
@@ -1,21 +1,40 @@
 #ifndef RESULTCOMPARATOR_H
 #define RESULTCOMPARATOR_H
 
+#include <map>
+#include <string>
+
 //TODO implement this
 class ResultComparator
 {
 public:
-  ResultComparator();
+  typedef std::map<std::string,double> feature_grade_map_t;
+
+  virtual double operator()(const feature_grade_map_t&) = 0;
+
+protected:
+  ResultComparator(const feature_grade_map_t& gradeRates);
+  feature_grade_map_t gradeRates_;
 };
 
 class AndComparator : public ResultComparator
 {
-//TODO implement this
+public:
+  AndComparator(const feature_grade_map_t& gradeRates);
+  virtual double operator()(const feature_grade_map_t&);
+
+private:
+  double evaluateGrades(const feature_grade_map_t& featureGrades);
 };
 
 class OrComparator : public ResultComparator
 {
-//TODO implement this
+public:
+  OrComparator(const feature_grade_map_t& gradeRates);
+  virtual double operator()(const feature_grade_map_t&);
+
+private:
+  double evaluateGrades(const feature_grade_map_t& featureGrades);
 };
 
 // TODO implement this
